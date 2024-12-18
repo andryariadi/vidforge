ee6e09451ece126a4836282e025bcca81353fbf9
Andry Ariadi <andryariadi23@gmail.com>
2024-12-18 16:11:27 +0700

    setup nextjs tailwind next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  // experimental: {
+  //   serverActions: {
+  //     bodySizeLimit: "5mb",
+  //   },
+  // },
 };
 
 export default nextConfig;
