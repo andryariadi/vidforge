2eb20a16db4c4a84d39289533af3be1467905d7e
Andry Ariadi <andryariadi23@gmail.com>
2024-12-19 16:30:58 +0700

    setup clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -19,11 +20,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${manrope.className} antialiased text-white-1`}>
-        {children}
-        <Toaster position="top-right" />
-      </body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+      appearance={{
+        layout: {
+          socialButtonsVariant: "iconButton",
+          logoImageUrl: "/logo.svg",
+        },
+        variables: {
+          colorBackground: "#15171c",
+          colorPrimary: "",
+          colorText: "white",
+          colorInputBackground: "#1b1f29",
+          colorInputText: "white",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${manrope.className} antialiased text-white-1`}>
+          {children}
+          <Toaster position="top-right" />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
